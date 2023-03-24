eed8a62c5d1d6e611b77c576a6d0ab973bc9a63f
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-03-24 09:51:55 +0100

    Build: set-up detail-page JS

---
+++ routes/plantNew.js
@@ -0,0 +1,22 @@
+import * as dotenv from "dotenv";
+import { fetchJson, postJson } from "../helpers/fetchWrapper.js";
+import express from "express";
+
+dotenv.config();
+
+const plantRoute = express.Router();
+
+// STEKJE DETAIL PAGE ROUTE
+plantRoute.get("/stek", (request, response) => {
+  const id = request.query.id;
+  if (!id) {
+    response.redirect("/");
+  }
+  const url = `${process.env.API_URL}/stekjes/?id=${id}`;
+
+  fetchJson(url).then((data) => {
+    response.render("stekje", data);
+  });
+});
+
+export default plantRoute;
