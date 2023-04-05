4216476eabb372a8fe34ebf63f6a37660d82aefc
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-04-05 11:10:02 +0200

    Fix: issue Id
    Mijn hele pagina werkte niet omdat ik mijn eindpoint url een id heb meegegeven, Maar mijn hoofdpagina hoeft geen id op te halen. @Suus Harsveld heeft mij hierbij geholpen, waardoor mijn issue is opgelost.

--- routes/plantNew.js
+++ routes/plantNew.js
@@ -1,22 +1,29 @@
 import * as dotenv from "dotenv";
-import { fetchJson } from "../helpers/fetchWrapper.js";
+import { postJson } from "../helpers/fetchWrapper.js";
 import express from "express";
 
 dotenv.config();
 
 const plantRoute = express.Router();
 
-// STEKJE DETAIL PAGE ROUTE
-plantRoute.get("/stek", (request, response) => {
-  const id = request.query.id;
-  if (!id) {
-    response.redirect("/");
-  }
-  const url = `${process.env.API_URL}/stekjes/?id=${id}`;
+plantRoute.get("/", (request, response) => {
+  response.render("plantNew");
+});
 
-  fetchJson(url).then((data) => {
-    response.render("stekje", data);
+//  Roep de API aan met de post methode en post in de API, die vervolgens wordt getoond in de index.ejs waar de get plaats vindt.
+plantRoute.post("/", (request, response) => {
+  const baseUrl = `${process.env.API_URL}/stekjes`;
+
+  postJson(baseUrl, request.body).then((data) => {
+    if (data.success) {
+      response.redirect("/"); // plant meegeven, message meegeven
+      // Toon opnieuw het formulier (met waarden) als het niet gelukt is
+    } else {
+      response.render("/aanmelden", error); // Fail, message meegeven
+    }
   });
+  //  De waarden uit het formulier (niet de API)
+  console.log(JSON.stringify(data));
 });
 
 export default plantRoute;
