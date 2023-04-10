5e95d105c0a2974aeb7195daed4fa6ff45d2a7f3
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-04-10 11:57:00 +0200

    Refactor: post-code V2

--- routes/plantNew.js
+++ routes/plantNew.js
@@ -1,30 +1,35 @@
 import * as dotenv from "dotenv";
-import { postJson } from "../helpers/fetchWrapper.js";
+import { postJson, fetchJson } from "../helpers/fetchWrapper.js";
 import express from "express";
 
 dotenv.config();
 
 const plantRoute = express.Router();
 
+// haalt planten gegevens(data) op uit API
 plantRoute.get("/", (request, response) => {
-  //Deze code zorgt ervoor dat wanneer er een GET-request naar de "/" route van de plantNew.js komt, de "plantNew" sjabloon wordt gerenderd en als een plantNew.ejs wordt geretourneerd.
   response.render("plantNew");
 });
 
-//  Roep de API aan met de post methode en post in de API, die vervolgens wordt getoond in de index.ejs waar de get plaats vindt.
+// Toon het formulier om een nieuw plantje te doneren
+plantRoute.get("/aanmelden", (request, response) => {
+  response.render("plantNew.ejs");
+});
+
+// Handel het versturen van het formulier af
+// Roep de API aan met de post methode en post in de API, die vervolgens wordt getoond in de index.ejs waar de get plaats vindt.
 plantRoute.post("/", (request, response) => {
   const baseUrl = `${process.env.API_URL}/stekjes`;
-
   postJson(baseUrl, request.body).then((data) => {
     if (data.success) {
       response.redirect("/"); // plant meegeven, message meegeven
       // Toon opnieuw het formulier (met waarden) als het niet gelukt is
     } else {
-      response.render("/aanmelden", error); // Fail, message meegeven
+      response.render("plantNew.ejs", { error: data.error }); // Fail, message meegeven
     }
+    //   //  De waarden uit het formulier (niet de API)
+    console.log(JSON.stringify(request.body));
   });
-  //  De waarden uit het formulier (niet de API)
-  console.log(JSON.stringify(data));
 });
 
 export default plantRoute;
